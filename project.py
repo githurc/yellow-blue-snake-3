bdd1625f6cc8d7450b640d7eb1f986cf1bbec114
マオ <inbox.arthur@gmail.com>
2020-07-26 11:23:42 +0800

    Updated with comments and removed sensitive info

--- project.py
+++ project.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # The Project #
-# 1. This is a project with minimal scaffolding. Expect to use the the discussion forums to gain insights! It’s not cheating to ask others for opinions or perspectives!
-# 2. Be inquisitive, try out new things.
-# 3. Use the previous modules for insights into how to complete the functions! You'll have to combine Pillow, OpenCV, and Pytesseract
-# 4. There are hints provided in Coursera, feel free to explore the hints if needed. Each hint provide progressively more details on how to solve the issue. This project is intended to be comprehensive and difficult if you do it without the hints.
-# 
-# ### The Assignment ###
-# Take a [ZIP file](https://en.wikipedia.org/wiki/Zip_(file_format)) of images and process them, using a [library built into python](https://docs.python.org/3/library/zipfile.html) that you need to learn how to use. A ZIP file takes several different files and compresses them, thus saving space, into one single file. The files in the ZIP file we provide are newspaper images (like you saw in week 3). Your task is to write python code which allows one to search through the images looking for the occurrences of keywords and faces. E.g. if you search for "pizza" it will return a contact sheet of all of the faces which were located on the newspaper page which mentions "pizza". This will test your ability to learn a new ([library](https://docs.python.org/3/library/zipfile.html)), your ability to use OpenCV to detect faces, your ability to use tesseract to do optical character recognition, and your ability to use PIL to composite images together into contact sheets.
-# 
-# Each page of the newspapers is saved as a single PNG image in a file called [images.zip](./readonly/images.zip). These newspapers are in english, and contain a variety of stories, advertisements and images. Note: This file is fairly large (~200 MB) and may take some time to work with, I would encourage you to use [small_img.zip](./readonly/small_img.zip) for testing.
-# 
-# Here's an example of the output expected. Using the [small_img.zip](./readonly/small_img.zip) file, if I search for the string "Christopher" I should see the following image:
-# ![Christopher Search](./readonly/small_project.png)
-# If I were to use the [images.zip](./readonly/images.zip) file and search for "Mark" I should see the following image (note that there are times when there are no faces on a page, but a word is found!):
-# ![Mark Search](./readonly/large_project.png)
-# 
-# Note: That big file can take some time to process - for me it took nearly ten minutes! Use the small one for testing.
+# ##  1. Import packages and define basics for data:#
 
 # In[2]:
 
@@ -32,18 +16,19 @@ import numpy as np
 # loading the face detection classifier
 face_cascade = cv.CascadeClassifier('readonly/haarcascade_frontalface_default.xml')
 
-# the rest is up to you!
+# declare dictionaries to provide structures for data storage and searching
+page_img_dic = {} # relate page file name and the scanned image of the page
+page_text_dic = {} # relate page file name and text of the page
+page_faces_dic = {} # relate page file name and images of faces of the page
 
-
-# Open Zip File and set up basic dictionaries for later use:
-
-# In[27]:
-
-
-page_text_dic = {}
-page_faces_dic = {}
-page_img_dic = {}
+# set up Image font type
 font = ImageFont.truetype(r'readonly/fanwood-webfont.ttf', 24) 
+
+
+# ## 2. Open zip file and assign relevant data to dictionaries:
+
+# In[ ]:
+
 
 with ZipFile('readonly/images.zip', 'r') as img_source:
     p_lst = img_source.infolist()    
@@ -56,27 +41,30 @@ with ZipFile('readonly/images.zip', 'r') as img_source:
         page_faces_dic[f] = "" 
         
 
+# to test file data retrieval:
 # print (p_lst[0])
 # test_img = page_img_dic.get('a-0.png')
 # display (test_img)
-print(page_img_dic)
+# print(page_img_dic)
 
 
-# Proceed with "page to text" and save the strings in page_text_dic:
+# ## 3. Proceed with "page to text" handling and save as strings in page_text_dic:
 
-# In[28]:
+# In[ ]:
 
 
 for k, v in page_img_dic.items():
     text = pytesseract.image_to_string(v)
     page_text_dic[k] = text
 
-print (page_text_dic)
+# to test if data was saved in page_text_dic:
+# t = page_text_dic.get('a-0.png')
+# print (t)
 
 
-# Proceed with "page to face" and save the face images in page_faces_dic:
+# ## 4. Proceed with "page to face" handling and save lists of face images in page_img_dic:
 
-# In[29]:
+# In[ ]:
 
 
 for k, v in page_img_dic.items():
@@ -91,13 +79,14 @@ for k, v in page_img_dic.items():
         face_lst.append(face)
     
     page_faces_dic[k] = face_lst
+    
+# to test if data was saved in page_faces_dic:
+# print (page_faces_dic.get('a-1.png'))
 
-print (page_faces_dic.get('a-1.png'))
 
+# ## 5. Define a function that generates different images for pages with/ without faces:
 
-# Define a function to return contact sheet:
-
-# In[30]:
+# In[ ]:
 
 
 def face_results(filename):
@@ -136,12 +125,14 @@ def face_results(filename):
         return (face_result)
     
 
+# to test if different images can be generated for different pages
+# a = 'a-0.png'    
+# face_results(a)
+# b = 'a-8.png'    
+# face_results(b)
 
-a = 'a-0.png'    
-face_results(a)
 
-
-# Define a function that would combine all result images into one image:
+# ## 6. Define a function that searches user queries and combine all results into one image:
 
 # In[35]:
 
@@ -167,7 +158,7 @@ def search():
         r_1 = result_img_lst[0]
         for i in result_img_lst:
             combined_h += i.height
-            results_img = Image.new("RGB", (500,combined_h))
+        results_img = Image.new("RGB", (500,combined_h))
         for i in result_img_lst:    
             results_img.paste(i, (x, y))
             y += i.height
@@ -175,17 +166,23 @@ def search():
         
 
 
+# ## Test 1: Try searching with the word "Mark":
+
 # In[36]:
 
 
 search()
 
 
+# ## Test 2: Try searching with the word "Christopher":
+
 # In[37]:
 
 
 search()
 
+
+# ## Test 3: Try searching with the word "hard":
 
 # In[38]:
 
